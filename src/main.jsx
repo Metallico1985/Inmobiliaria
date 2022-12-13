84d933219f9bf4c6c6446dd8a4ef95dcfdbfa0b5
Metallico1985 <andrescardonalatorre@gmail.com>
2022-12-13 11:09:45 -0300

    protected route registro user

--- src/main.jsx
+++ src/main.jsx
@@ -39,7 +39,7 @@ const router = createBrowserRouter([
   },
   {
     path: "/registro",
-    element: <ProtectedRoute><FormRegistro /></ProtectedRoute>,
+    element: <FormRegistro />,
   },
   {
     path: "/login",
